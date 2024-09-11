89f6f30105b06785a72174a894f860183f7a28d3
João Uchoa <joaovictoruchoa01@gmail.com>
2024-09-11 20:52:58 -0300

    att

---
+++ aula04/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Exercicio1 from './components/Exercicio1'
+import Exercicio2 from './components/Exercicio2'
+import Campanha from './components/Campanha'
+
+const App = () => {
+  return (
+    <div>
+      <Campanha mes = "novembro"/>
+    </div>
+  )
+}
+
+export default App
---
+++ aula04/src/components/Exercicio2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Exercicio2 = () => {
+  return (
+    <div style={{ color: '#ff0000', backgroundColor:'#0000ff', width: '200px', textAlign: 'center'}}>
+    
+        Isso não é um teste
+    
+    </div>
+
+
+  )
+}
+
+export default Exercicio2
