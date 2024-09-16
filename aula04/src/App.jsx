1831e1cdae93947f96f1f717c244f1abe2bc57af
João Uchoa <joaovictoruchoa01@gmail.com>
2024-09-16 12:48:38 -0300

    componentsFormularioDeContato

--- aula04/src/App.jsx
+++ aula04/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Exercicio1 from './components/Exercicio1'
 import Exercicio2 from './components/Exercicio2'
 import Campanha from './components/Campanha'
+import FormularioDeContato from './components/FormularioDeContato'
 
 const App = () => {
   return (
     <div>
-      <Campanha mes = "novembro"/>
+      <FormularioDeContato/>
     </div>
   )
 }
